d2b43aafe0c6b1f3fd9a2df134fb74f3d8518153
TN1608 <120995414+TN1608@users.noreply.github.com>
2024-04-06 12:44:18 +0700

    THem thanh vien

---
+++ src/main/java/dao/ThanhVienDAO.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import entities.ThanhVien;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author NGUYEN THI NGUYET VY
+ */
+public class ThanhVienDAO {
+       public void insert(ThanhVien model){
+       String sql="Insert into giohang values(?,?,?,?,?)";
+       utils.JDBCHelper.update(sql, 
+               model.getMaTV(),
+               model.getTenTV(),
+               model.getNgaysinh(),
+               model.isGioitinh(),
+               model.getDiem());
+   }
+   public void update(ThanhVien model){
+       String sql = "update ThanhVien set tenthanhvien = ?, ngaysinh = ?, gioitinh = ?, diem = ? where mathanhvien = ?";
+       utils.JDBCHelper.update(sql, 
+               model.getTenTV(),
+               model.getNgaysinh(),
+               model.isGioitinh(),
+               model.getDiem(),
+               model.getMaTV());
+    }
+    public void delete(int ma){
+        String sql="DELETE FROM ThanhVien WHERE maThanhVien = ?";
+        utils.JDBCHelper.update(sql, ma);
+    }
+    
+    public ThanhVien selectById(int ma){
+        String sql = "SELECT * FROM ThanhVien WHERE mathanhvien = ?";
+        List<ThanhVien> list = this.SelectBySQL(sql, ma);
+        return list.size() > 0 ? list.get(0) : null;
+    }
+    
+    public List<ThanhVien> SelectBySQL(String sql, Object... args) {
+        List<ThanhVien> listS = new ArrayList<>();
+        try {
+            ResultSet rs = null;
+            try {
+                rs = utils.JDBCHelper.query(sql, args);
+                while (rs.next()) {
+                    ThanhVien st = new ThanhVien();
+                    st.setMaTV(rs.getInt(1));
+                    st.setTenTV(rs.getString(2));
+                    st.setNgaysinh(rs.getDate(3));
+                    st.setGioitinh(rs.getBoolean(4));
+                    st.setDiem(rs.getInt(5));
+                    listS.add(st);
+                }
+            } finally {
+                rs.getStatement().getConnection().close();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return listS;
+    }
+    public List<ThanhVien> SelectAll() {
+        String sql = "SELECT * FROM thanhvien";
+        return SelectBySQL(sql);
+    }
+}
---
+++ src/main/java/entities/ThanhVien.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+import java.util.Date;
+
+/**
+ *
+ * @author NGUYEN THI NGUYET VY
+ */
+public class ThanhVien {
+    private int maTV; // mathanhvien la sdt
+    private String tenTV;
+    private Date ngaysinh;
+    private boolean gioitinh; //Nam 0 - Nu 1
+    private int diem;
+
+    public ThanhVien() {
+    }
+
+    public ThanhVien(int maTV, String tenTV, Date ngaysinh, boolean gioitinh, int diem) {
+        this.maTV = maTV;
+        this.tenTV = tenTV;
+        this.ngaysinh = ngaysinh;
+        this.gioitinh = gioitinh;
+        this.diem = diem;
+    }
+
+    public int getMaTV() {
+        return maTV;
+    }
+
+    public void setMaTV(int maTV) {
+        this.maTV = maTV;
+    }
+
+    public String getTenTV() {
+        return tenTV;
+    }
+
+    public void setTenTV(String tenTV) {
+        this.tenTV = tenTV;
+    }
+
+    public Date getNgaysinh() {
+        return ngaysinh;
+    }
+
+    public void setNgaysinh(Date ngaysinh) {
+        this.ngaysinh = ngaysinh;
+    }
+
+    public boolean isGioitinh() {
+        return gioitinh;
+    }
+
+    public void setGioitinh(boolean gioitinh) {
+        this.gioitinh = gioitinh;
+    }
+
+    public int getDiem() {
+        return diem;
+    }
+
+    public void setDiem(int diem) {
+        this.diem = diem;
+    }
+    
+    
+}
